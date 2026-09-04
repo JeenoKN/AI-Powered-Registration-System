55a9f5ae6e5808f3303dd8aebd196ca19da4764d
JeenoKN <147079996+JeenoKN@users.noreply.github.com>
2026-09-05 03:12:00 +0700

    Remove remaining temporary migration and inspection artifacts

--- inspect_modal.py
+++
@@ -1,8 +0,0 @@
-import re
-with open(r'e:\NewSystem\frontend-vue\src\views\AdminView.vue', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-start = text.find('<!-- View Form Modal -->')
-if start != -1:
-    end = text.find('</Transition>', start)
-    print(text[start:end+15])
